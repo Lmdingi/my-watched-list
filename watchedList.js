5d769b1516bcf702762b622f107b5eed7575fb78
Luxolo Mdingi <lmdingi4@gmail.com>
2023-12-15 21:34:51 +0200

    add search button + one movie  Changes to be committed: 	modified:   index.html 	new file:   media/Meg_2-_The_Trench_film_cover.jpg 	modified:   style.css 	modified:   watchedList.js

--- watchedList.js
+++ watchedList.js
@@ -1,2 +1,6 @@
-function watchedList() {}
+function search(){
+    let getTitle = prompt("Enter Movie Title");
+    let movie = document.getElementById(getTitle)
+    movie.style.backgroundColor = "#044040";
+}
 module.exports = { watchedList };
